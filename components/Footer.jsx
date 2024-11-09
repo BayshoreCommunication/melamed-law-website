651164419abdcbed78d8543a3c605c84a7ddaf96
uiabu <ui.abukawsar@gmail.com>
2024-11-09 18:08:00 +0600

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -110,9 +110,13 @@ const Footer = () => {
                 <ul className="ml-0 text-[18px] font-medium text-black list-none">
                   {SITECONFIG?.footer?.company?.map((el, index) => (
                     <li className="mb-4" key={index}>
-                      <Link href={el.slug} className="hover:underline">
+                      {/* <Link href={el.slug} className="hover:underline">
                         {el.title}
-                      </Link>
+                      </Link> */}
+                      <p className="hover:underline cursor-pointer">
+                        {" "}
+                        {el.title}
+                      </p>
                     </li>
                   ))}
                 </ul>
