f50ea90060eb9e8f2776de12f77fb816622349be
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2025-01-08 17:53:28 +0600

    fixed bug

--- components/MainNavbar.jsx
+++ components/MainNavbar.jsx
@@ -41,18 +41,18 @@ const debounce = (func, wait) => {
 const MainNavbar = () => {
 
 
-var prevScrollpos = window.scrollY;
-window.onscroll = function() {
-var currentScrollPos = window.scrollY;
+// var prevScrollpos = window.scrollY;
+// const nav=window.onscroll = function() {
+// var currentScrollPos = window.scrollY;
 
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navbar").style.top = "0";
-  } else {
-    document.querySelector(".navbar").style.top = "-200px";
-  }
-  prevScrollpos = currentScrollPos;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector(".navbar").style.top = "0";
+//   } else {
+//     document.querySelector(".navbar").style.top = "-200px";
+//   }
+//   prevScrollpos = currentScrollPos;
 
-}
+// }
 
 
 
