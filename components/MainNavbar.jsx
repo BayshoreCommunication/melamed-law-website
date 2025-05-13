4133cd50c3f1f983feb1529b12fcce89cb22a15f
uiabu <ui.abukawsar@gmail.com>
2025-05-13 15:35:05 +0600

    Update Footer component to display the current year dynamically

--- components/MainNavbar.jsx
+++ components/MainNavbar.jsx
@@ -101,35 +101,6 @@ const MainNavbar = () => {
   return (
     <section className={"relative z-50 "}>
       <div className="bg-primary w-full navbar ">
-        {/* <div className="hidden lg:block">
-          <div className="container flex justify-between">
-            <div className="bg-secondary  py-2 px-5">
-              <p className="text-white text-[10px]  xl:text-lg">
-                Call For A Free Consultation
-              </p>
-              <div className="flex items-center gap-x-2">
-                <BsTelephoneForwardFill className="text-white size-4 xl:size-5" />
-                <h6 className="text-white font-bold text-base  xl:text-2xl">
-                  954-529-0695
-                </h6>
-              </div>
-            </div>
-            <div className="flex items-center justify-center gap-x-4">
-              <div className="flex items-center gap-x-1">
-                <HiOutlineMail className="text-white size-4 xl:size-6" />
-                <h6 className="text-white font-normal text-base xl:text-lg">
-                  {` info@melamedlaw.com`}
-                </h6>
-              </div>
-              <div className="flex items-center gap-x-1">
-                <IoLocationSharp className="text-white size-4 xl:size-6" />
-                <h6 className="text-white font-normal text-base xl:text-lg">
-                  {` 3040 NE 190th St APT 303, Aventura, FL 33180, United States`}
-                </h6>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div className="bg-white hidden md:block">
           <div className="flex items-center container  justify-between">
             <div className=" flex items-center gap-x-10 2xl:gap-x-16">
@@ -158,7 +129,6 @@ const MainNavbar = () => {
                         <div className="flex items-center gap-1 ">
                           <Link
                             href={el?.slug}
-                            // className={`flex text-slate-900 font-semibold text-lg hover:border-b-2 hover:border-solid hover:border-black ${pathname === el?.slug ? "border-b-2 border-solid border-[#0B7788] text-[#0B7788]" : ""}`}
                             className={`flex items-center gap-x-1 cursor-pointer text-black text-sm xl:text-[16px] font-medium capitalize hover:text-secondary ${pathname === el.slug || setPathSlug ? " border border-secondary rounded-full text-secondary py-1 lg:py-2 px-2 lg:px-5" : ""} `}
                           >
                             Practice Areas
@@ -178,20 +148,6 @@ const MainNavbar = () => {
                             </span>
                           </Link>
                         </div>
-                        {/* <div className="absolute hidden group-hover:block bg-white shadow-lg rounded p-6  overflow-y-scroll w-[300px] max-h-[400px]">
-                          <ul className="py-2 list-none ml-0">
-                            {areaspracticeData?.map((el, index) => (
-                              <li key={index}>
-                                <Link
-                                  href={`/practice-areas/${el.slug}`}
-                                  className={`py-2 flex  text-base xl:text-[16px] hover:text-secondary  ${pathname === `/practice-areas/${el?.slug}` ? " text-secondary " : "text-slate-900"}`}
-                                >
-                                  {el.title}
-                                </Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </div> */}
                       </div>
                     </div>
                   ) : (
@@ -208,24 +164,14 @@ const MainNavbar = () => {
             </div>
 
             <div className="">
-              {/* <Link
-                href={"/contact"}
-                className="text-white font-normal text-lg bg-secondary px-2 xl:px-16 py-2 xl:py-4 rounded-full hover:bg-orange-400"
-              >
-                Contact Us
-              </Link> */}
               <div className="bg-secondary  py-2  md:px-1 lg:px-1 xl:px-2">
                 <Link
                   href="tel:954-529-0695"
                   className="flex flex-col justify-center"
                 >
-                  <p className="text-white text-[6px]  xl:text-[12px] text-center">
-                    CONTACT US FOR A FREE CONSULTATION <br />
-                    (Se Habla Español)
-                  </p>
                   <div className="flex items-center justify-center gap-x-0 xl:gap-x-2">
                     <BsTelephoneForwardFill className="text-white size-4  xl:size-5" />
-                    <h6 className="text-white font-bold text-sm lg:text-base xl:text-xl">
+                    <h6 className="text-white font-bold text-sm lg:text-base ">
                       954-529-0695
                     </h6>
                   </div>
